ac765043d8481c6070891682b5832ffa6dd27eb0
NILANJANA DEY <nilanjanadey276@gmail.com>
2023-12-18 16:09:19 +0530

    initial commit

---
+++ js/data.js
@@ -0,0 +1,24 @@
+const people = [
+    {
+    img:
+    "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959121/person-1_aufeoq.jpg",
+    name: "peter doe",
+    job: "LOVESTORY",
+    text: `A sweet and overwhelming movie with lots of drama anf romance. A must-watch film for all teens and couples. `,
+    },
+    {
+    img:
+    "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-2_ipcjws.jpg",
+    name: "Chitra Roy",
+    job: "RRR",
+    text: `A peek into the past, when the Britishers oppressed the humans. This patriotic movie focuses on the roller-coaster of human emotions but also paints the true value of friendship and love in times of peril. `,
+    },
+    {
+    img:
+    "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-3_rxtqvi.jpg",
+    name: "Sabitri Bhosle",
+    job: "V THE MOVIE",
+    text: `Honestly...an average one....good for thriller lovers but not for people who looks for good plot`,
+    },
+    ];
+    export default people;
